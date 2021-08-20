8423624d8d5d2c5ffb60056a603822afc4ac8c3c
杨尉文 <1252610144@qq.com>
2021-08-20 18:40:14 +0800

    创建header.vue组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
-
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
+// 在header中并不需要export name:'AppHeader',只需在使用该组件的时候为它命名即可
+// 在Vue中,驼峰式命名在template中用小写+'-'的方式连接,Vue中是不区分大小写的
+import AppHeader from "./components/head/header";
 
 export default {
   name: 'App',
   components: {
-
+    AppHeader
   }
 }
 </script>
